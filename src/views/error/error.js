bde7eba15b07bec6f5b8bd4879c74ffc855aca16
aidanbeale <aidanbeale@gmail.com>
2023-04-22 14:49:26 +1000

    Add react-router

---
+++ src/views/error/error.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useRouteError } from "react-router-dom";
+
+import './error.css';
+
+function Error() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  )
+}
+
+export default Error;
