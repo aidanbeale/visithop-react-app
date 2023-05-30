970ebb8bfa8e168cb4ffcbb2c81ed6d3cbdb7193
aidanbeale <aidanbeale@gmail.com>
2023-05-30 13:06:16 +1000

    Fix buttons

--- src/components/loginbutton/loginbutton.js
+++ src/components/loginbutton/loginbutton.js
@@ -5,7 +5,7 @@ import "./loginbutton.css";
 const LoginButton = () => {
   return (
     <a
-      to="https://visithop.auth.ap-southeast-2.amazoncognito.com/authorize?client_id=63vcmquos72i3tt3lc1k8tjkfr&response_type=code&redirect_uri=http://localhost:3000/"
+      href="https://visithop.auth.ap-southeast-2.amazoncognito.com/authorize?client_id=63vcmquos72i3tt3lc1k8tjkfr&response_type=code&redirect_uri=http://localhost:3000/"
     >
       <button variant="contained" className="btn-login">
         Log In
--- src/components/signupbutton/signupbutton.js
+++ src/components/signupbutton/signupbutton.js
@@ -6,7 +6,6 @@ const SignupButton = () => {
   return (
     <a
       href="https://visithop.auth.ap-southeast-2.amazoncognito.com/authorize?client_id=63vcmquos72i3tt3lc1k8tjkfr&response_type=code&redirect_uri=http://localhost:3000/"
-      className="btn-signup"
     >
       <button variant="contained" className="btn-signup">
         Sign Up
