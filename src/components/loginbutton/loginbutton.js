9558ebff107e1be7b8544f9d7bee9d1e01414568
aidanbeale <aidanbeale@gmail.com>
2023-05-31 15:37:30 +1000

    Update login/signup links

--- src/components/loginbutton/loginbutton.js
+++ src/components/loginbutton/loginbutton.js
@@ -15,7 +15,7 @@ const LoginButton = ({ isHome }) => {
 
   return (
     <a
-      href="https://visithop.auth.ap-southeast-2.amazoncognito.com/authorize?client_id=63vcmquos72i3tt3lc1k8tjkfr&response_type=code&redirect_uri=http://localhost:3000/"
+      href="https://visithop.auth.ap-southeast-2.amazoncognito.com/login?client_id=63vcmquos72i3tt3lc1k8tjkfr&response_type=code&redirect_uri=http://localhost:3000/"
     >
       <button variant="contained" className="btn-login" ref={loginRef}>
         Log In
